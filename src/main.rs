eef3f90e7801765911026b9bff7c41581d5f0889
yinho999 <yinho999@gmail.com>
2023-02-12 22:22:07 +0000

    fixed compiled problem

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main] // disable all Rust-level entry points
 use core::panic::PanicInfo;
 
+// Requires nightly to compile
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
